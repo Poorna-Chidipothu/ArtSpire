be542c5f370815576e10047b835c84fd6baef391
Poorna Chidipothu <vgvspd@gmail.com>
2024-08-06 18:55:47 +0530

    Backend Updated.

--- Backend/controllers/imageController.js
+++ Backend/controllers/imageController.js
@@ -1,57 +1,6 @@
-// import { uploadImageOnCloudinary } from "../helper/cloudinaryHelper.js";
-// import Image from "../models/imageModel.js";
-// import { generateImageHash } from "../utils/hashImage.js";
-// const addImageController = async (req, res) => {
-//     try{
-//         const userId = req.user._id;
-//         const files = req.files;
-//         const {tags} = req.body;
-        
-//         const imageUploadPromises = files.map(async (file) => {
-//             const hash = await generateImageHash(file.path);
-
-//             // Check if the hash already exists in the database
-//             const existingImage = await Image.findOne({ hash });
-//             if (existingImage) {
-//                 return { error: 'Image already exists', existingImage };
-//             }
-
-//             const uploadedImage = await uploadImageOnCloudinary(file.path, 'Images');
-
-//             return {
-//                 picture: {
-//                     picture_url: uploadedImage.secure_url,
-//                     public_id: uploadedImage.public_id,
-//                 },
-//                 uploadedBy: userId,
-//                 hash,
-//                 tags: tags ? tags.split(',') : [],
-//             };
-//         });
-
-//         const results = await Promise.all(imageUploadPromises);
-
-//         // Filter out images that already exist
-//         const newImages = results.filter(result => !result.error);
-
-//         if (newImages.length > 0) {
-//             const savedImages = await Image.insertMany(newImages);
-//             res.status(200).json(savedImages);
-//         } else {
-//             res.status(409).json({ message: 'All uploaded images already exist' });
-//         }
-//     }catch(error){
-//         console.log(error);
-//         return res.json({success : false,message :'image Upload Error'})
-//     }
-// }
-
-// export {addImageController}
-
 import { uploadImageOnCloudinary } from '../helper/cloudinaryHelper.js';
 import { generateImageHash } from '../utils/hashImage.js';
 import Image from '../models/imageModel.js';
-// import {rimraf} from 'rimraf';
 import fs from 'fs';
 import path from 'path';
 
--- Backend/helper/cloudinaryHelper.js
+++ Backend/helper/cloudinaryHelper.js
@@ -1,5 +1,4 @@
 import cloudinary from '../config/cloudinaryconfig.js';
-import { rimraf } from 'rimraf';
 
 const uploadImageOnCloudinary = async (filePath, folderName) => {
     try {
@@ -7,13 +6,6 @@ const uploadImageOnCloudinary = async (filePath, folderName) => {
             folder: folderName
         });
         console.log('Uploaded to Cloudinary:', result);
-
-        // // Delay before attempting to delete the file
-        // setTimeout(() => {
-        //     rimraf(filePath, { glob: false })
-        //         .then(() => console.log('File deleted from server:', filePath))
-        //         .catch((error) => console.error('Failed to delete the file from server using rimraf', error));
-        // }, 10000); // 10-second delay
 
         return result;
     } catch (error) {
